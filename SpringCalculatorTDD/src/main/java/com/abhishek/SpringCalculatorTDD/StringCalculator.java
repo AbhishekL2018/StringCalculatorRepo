a317047e4ce022e895b741dd455094c19ef53ec7
AbhishekL2018 <abhishekkar647@gmail.com>
2020-12-29 16:50:38 +0530

    7 th commit: two numbers comma seperated sum test - success

--- SpringCalculatorTDD/src/main/java/com/abhishek/SpringCalculatorTDD/StringCalculator.java
+++ SpringCalculatorTDD/src/main/java/com/abhishek/SpringCalculatorTDD/StringCalculator.java
@@ -3,15 +3,21 @@ package com.abhishek.SpringCalculatorTDD;
 public class StringCalculator {
 
 	public int calculate(String number) {
-		if(isEmpty(number)) {
+		String[] inputNumbers = number.split(",");
+		if (isEmpty(number)) {
 			return 0;
 		}
-		return stringToInteger(number);
+		if (number.length() == 1) {
+			return stringToInteger(number);
+		} else {
+			return Integer.parseInt(inputNumbers[0]) + Integer.parseInt(inputNumbers[1]);
+		}
 	}
-	
+
 	private boolean isEmpty(String number) {
 		return number.isEmpty();
 	}
+
 	private int stringToInteger(String number) {
 		return Integer.parseInt(number);
 	}
