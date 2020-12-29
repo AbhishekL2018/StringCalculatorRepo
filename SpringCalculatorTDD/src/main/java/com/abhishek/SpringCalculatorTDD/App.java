67814a31aab376d0b258c7ce46c5108507f9d100
AbhishekL2018 <abhishekkar647@gmail.com>
2020-12-29 15:11:10 +0530

    first commit: project setup

---
+++ SpringCalculatorTDD/src/main/java/com/abhishek/SpringCalculatorTDD/App.java
@@ -0,0 +1,13 @@
+package com.abhishek.SpringCalculatorTDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
