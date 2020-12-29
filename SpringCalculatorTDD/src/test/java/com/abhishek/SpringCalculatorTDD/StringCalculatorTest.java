8cf7b945608a9c599b1fb22ac8d96a81d8b02ffa
AbhishekL2018 <abhishekkar647@gmail.com>
2020-12-29 16:03:16 +0530

    4 th commit: single number test - failure

--- SpringCalculatorTDD/src/test/java/com/abhishek/SpringCalculatorTDD/StringCalculatorTest.java
+++ SpringCalculatorTDD/src/test/java/com/abhishek/SpringCalculatorTDD/StringCalculatorTest.java
@@ -2,14 +2,26 @@ package com.abhishek.SpringCalculatorTDD;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 	
+	private StringCalculator calculator;
+	
+	@Before
+	public void init() {
+		calculator = new StringCalculator();
+	}
+	
 	@Test
 	public void emptyStringTest() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(calculator.calculate(""), 0);
+	}
+	
+	@Test
+	public void singleValueTest() {
+		assertEquals("1", 1);
 	}
 
 }
